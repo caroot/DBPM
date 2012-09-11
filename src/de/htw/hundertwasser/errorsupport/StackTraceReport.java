9389807052bd6ee65be3d09a93d7abab6ff77827
Devron <danielrhein84@googlemail.com>
2012-09-12 01:25:24 +0200

    Unnötige Referenzen entfernt.

--- src/de/htw/hundertwasser/errorsupport/StackTraceReport.java
+++ src/de/htw/hundertwasser/errorsupport/StackTraceReport.java
@@ -5,8 +5,6 @@ import java.awt.Toolkit;
 import java.awt.datatransfer.Clipboard;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.ComponentEvent;
-import java.awt.event.ComponentListener;
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
