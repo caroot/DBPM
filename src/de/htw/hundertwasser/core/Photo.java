c3a8ce04d2d3d8cc3a5d9f285026cb8411c3570a
Devron <danielrhein84@googlemail.com>
2012-09-11 14:43:49 +0200

    Photo-Basis hinzugefügt. Tim bitte mache diese Klasse fertig

---
+++ src/de/htw/hundertwasser/core/Photo.java
@@ -0,0 +1,13 @@
+package de.htw.hundertwasser.core;
+/**
+ * Diese Klasse abstrahiert ein  Photo
+ * Es muss nur den Pfad zur Datei kennen die es Anzeigen soll.
+ * Die Anzeige selbst wird mit einem anderen Objekt dem ImageManager realisiert.
+ * @author daniel
+ *
+ */
+public class Photo {
+
+	private String pathToFile = "";
+	private String name="";
+}
