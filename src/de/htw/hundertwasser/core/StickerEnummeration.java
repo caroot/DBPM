d5f13aaf17075e3af89a0e6a4b932b4881536d11
Devron <danielrhein84@googlemail.com>
2012-09-12 01:39:36 +0200

    StickerEnummeration hinzugefügt.

---
+++ src/de/htw/hundertwasser/core/StickerEnummeration.java
@@ -0,0 +1,15 @@
+package de.htw.hundertwasser.core;
+/**
+ * Anzuzeigende Sticker in einem PhotoAlbum
+ * @author daniel
+ *
+ */
+public enum StickerEnummeration {
+SMILEY,
+HEART,
+STAR,
+CIRCLE,
+BOX,
+ELIPSE,
+RECTANGLE;
+}
