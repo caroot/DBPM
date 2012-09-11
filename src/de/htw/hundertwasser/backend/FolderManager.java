99befb6df75c988557425729d5439e363c13f6f4
Devron <danielrhein84@googlemail.com>
2012-09-12 01:01:07 +0200

    FolderManager erstellt. Dieser gibt den Zugriff auf das Homeverzeichnis des Benutzers frei. Zudem ermöglicht er den Zugriff auf die Umgebungsvariablen "APPDATA". Es wäre nett wenn jemand mal testen könnte, ob Appdata abgefragt werden kann.

---
+++ src/de/htw/hundertwasser/backend/FolderManager.java
@@ -0,0 +1,49 @@
+package de.htw.hundertwasser.backend;
+
+import java.util.ArrayList;
+import java.util.Map;
+import java.util.Set;
+
+import de.htw.hundertwasser.core.EnvironmentChecker;
+
+/**
+ * This class will manage the Folders of the current Project
+ * 
+ * @author daniel
+ *
+ */
+public class FolderManager {
+
+	private EnvironmentChecker environmentChecker;
+	
+	public FolderManager()
+	{
+		environmentChecker = new EnvironmentChecker();
+		
+	}
+	/**
+	 * Show the Application path
+	 * @return
+	 */
+	public String getApplicationPath()
+	{
+		return System.getenv("APPDATA");
+	}
+	
+	
+	public Set<String> getEnvironmentKeyValues()
+	{
+		Map<String,String> keymap =System.getenv();
+		return 	keymap.keySet();
+	}
+	
+	public String getEnvironmentKeyValue(String key)
+	{
+		return System.getenv(key);
+	}
+	
+	public String getUsersHomePath()
+	{
+		return environmentChecker.getProperty("user.home");
+	}
+}
