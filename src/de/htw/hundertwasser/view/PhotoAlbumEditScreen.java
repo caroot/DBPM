e8e7ec0e4dca83485f73bad50d1d7e0e09a6b528
Ramis <Ramis@134.96.168.27>
2012-09-14 14:15:27 +0200

    PAES Implementiert

--- src/de/htw/hundertwasser/view/PhotoAlbumEditScreen.java
+++ src/de/htw/hundertwasser/view/PhotoAlbumEditScreen.java
@@ -1,10 +1,34 @@
 package de.htw.hundertwasser.view;
 
+import java.awt.event.WindowEvent;
+
+import javax.swing.JOptionPane;
+
 public class PhotoAlbumEditScreen extends EditScreen{
+	
+	
 
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
+
+
+	
+	
+	
+	
+	
+	@Override
+	public void windowClosed(WindowEvent arg0) {
+		JOptionPane.showConfirmDialog(this, "Would you like to save the current Project" , "Confirm Exit", JOptionPane.QUESTION_MESSAGE,JOptionPane.YES_NO_OPTION);
+		dispose();
+		
+		
+	}
+
+
+
+
 
 }
