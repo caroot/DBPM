1f6634edd51946e7fea9729b2e967f35c552438f
Ramis <Ramis@134.96.170.219>
2012-09-18 14:47:46 +0200

    Angepasst(gelöscht) und es funktioniert!!

--- src/de/htw/hundertwasser/view/EditScreen.java
+++ src/de/htw/hundertwasser/view/EditScreen.java
@@ -1,35 +1,28 @@
 package de.htw.hundertwasser.view;
 
 import java.awt.BorderLayout;
-import java.awt.Color;
 import java.awt.Dimension;
-import java.awt.FlowLayout;
-import java.awt.GridBagLayout;
 import java.awt.GridLayout;
 import java.awt.Toolkit;
 import java.awt.event.WindowEvent;
 import java.awt.event.WindowListener;
+import java.awt.image.BufferedImage;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 
-import javax.swing.Icon;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
-import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.UIManager;
 import javax.swing.UnsupportedLookAndFeelException;
 
 import de.htw.hundertwasser.core.Infobar;
 import de.htw.hundertwasser.core.NavigationBar;
+import de.htw.hundertwasser.core.Photo;
 import de.htw.hundertwasser.core.ThumbnailBar;
 import de.htw.hundertwasser.core.ToolBar;
-import java.awt.GridBagConstraints;
-import java.awt.Insets;
-import javax.swing.GroupLayout;
-import javax.swing.GroupLayout.Alignment;
-import javax.swing.LayoutStyle.ComponentPlacement;
-import java.awt.event.ActionListener;
-import java.awt.event.ActionEvent;
+import de.htw.hundertwasser.custom.error.InsufficientPrivilegesException;
 
 /**
  * Klasse die den EditScreen anzeigt
@@ -60,74 +53,63 @@ public class EditScreen extends JFrame implements WindowListener {
 	protected JButton next;
 	protected ToolBar toolBar;
 	protected Infobar infoBar;
-	private JLabel middle;
+	private ImagePanel imgPanel;
 
 	public EditScreen() {
 
 		setPreferredSize(screenSize);
-		setResizable(true);		
+		setResizable(true);
 		setTitle(DBPM);
 		addWindowListener(this);
 
 		// Create Elements
 
-		navigationBar = new NavigationBar();	 // Navigation  Links
-		infoBar = new Infobar();				 // Information Rechts Oben
-		toolBar = new ToolBar();				 // Toolbar		Rechts Unten
-		thumbnailBar = new ThumbnailBar();		 // Thumbnail	Unten 
+		navigationBar = new NavigationBar(); // Navigation Links
+		infoBar = new Infobar(); // Information Rechts Oben
+		toolBar = new ToolBar(); // Toolbar Rechts Unten
+		thumbnailBar = new ThumbnailBar(); // Thumbnail Unten
+		imgPanel = new ImagePanel();
 
-//		title = new JLabel(DBPM, JLabel.CENTER);
+		// title = new JLabel(DBPM, JLabel.CENTER);
 		navigationBar = new NavigationBar();
 		infoBar = new Infobar();
 		toolBar = new ToolBar();
 		thumbnailBar = new ThumbnailBar();
-		
-		
-		
-		
-		
-		
+
 		// Panel, die den PictureViewer beinhaltet
 		centralPanel = new JPanel();
-		centralPanel.setLayout(new BorderLayout(10, 10)); // und LayoutManager hinzufügen
+		centralPanel.setLayout(new BorderLayout(10, 10)); // und LayoutManager
+															// hinzufügen
 		// Panel, der die NavigationBar beinhaltet
 		controlPanel = new JPanel();
-		controlPanel.setLayout(new BorderLayout(10, 10)); // und LayoutManager hinzufügen
+		controlPanel.setLayout(new BorderLayout(10, 10)); // und LayoutManager
+															// hinzufügen
 		// Panels, die die Buttons beinhaltet
-		buttonPanelleft = new JPanel(new GridLayout(10,0));
-		buttonPanelright= new JPanel(new GridLayout(10,0));
-		
-		
-			
+		buttonPanelleft = new JPanel(new GridLayout(10, 0));
+		buttonPanelright = new JPanel(new GridLayout(10, 0));
+
 		// Hinzufügen von INFOBAR und TOOLBAR in den CONTROLPANEL
 		controlPanel.add(infoBar, BorderLayout.NORTH);
 		controlPanel.add(toolBar, BorderLayout.CENTER);
 		// Hinzufügen der beiden Panels für Buttons
 		centralPanel.add(buttonPanelleft, BorderLayout.EAST);
 		centralPanel.add(buttonPanelright, BorderLayout.WEST);
-		middle = new JLabel("New label");
-		centralPanel.add(middle, BorderLayout.CENTER);
-		middle.setBackground(Color.BLACK);
-		middle.setIcon(new ImageIcon(EditScreen.class.getResource("/de/htw/hundertwasser/res/delete.png")));
-		middle.setBounds(40, 40, 230, 123);
+		centralPanel.add(imgPanel, BorderLayout.CENTER);
+
 		// Jeder Richtung etwas zuweisen
 		getContentPane().setLayout(new BorderLayout(10, 10));
 
-		//add(navigationBar,BorderLayout.WEST);
+		// add(navigationBar,BorderLayout.WEST);
 
-//		getContentPane().add(title, BorderLayout.NORTH);
+		// getContentPane().add(title, BorderLayout.NORTH);
 		// add(navigationBar,BorderLayout.WEST);
 		// Thumbnailbar muss noch implementiert werden
 		// add(thumbnailbar,BorderLayout.SOUTH);
 		getContentPane().add(controlPanel, BorderLayout.EAST);
 		getContentPane().add(centralPanel, BorderLayout.CENTER);
 
+		// attractivePanel = new JPanel(new BorderLayout(10, 10));
 
-
-		
-		//attractivePanel = new JPanel(new BorderLayout(10, 10));
-		
-				
 		// Button NEXT ( Rechts )
 		next = new JButton();
 		next.setToolTipText("Next");
@@ -136,11 +118,11 @@ public class EditScreen extends JFrame implements WindowListener {
 		buttonPanelleft.add(new JPanel());
 		buttonPanelleft.add(new JPanel());
 		buttonPanelleft.add(next);
-		//centralPanel.add(next, BorderLayout.EAST); // auf Panel hinzufügen	
+		// centralPanel.add(next, BorderLayout.EAST); // auf Panel hinzufügen
 		// Button mit Pfeil versehen
-		next.setIcon(new ImageIcon(EditScreen.class.getResource("/de/htw/hundertwasser/res/arrow_right.png")));
-		
-		
+		next.setIcon(new ImageIcon(EditScreen.class
+				.getResource("/de/htw/hundertwasser/res/arrow_right.png")));
+
 		// Button Previous ( Links )
 		previous = new JButton();
 		previous.setToolTipText("Back");
@@ -149,21 +131,14 @@ public class EditScreen extends JFrame implements WindowListener {
 		buttonPanelright.add(new JPanel());
 		buttonPanelright.add(new JPanel());
 		buttonPanelright.add(previous);
-		//centralPanel.add(previous, BorderLayout.WEST); // auf Panel hinzufügen
+		// centralPanel.add(previous, BorderLayout.WEST); // auf Panel
+		// hinzufügen
 		// Button mit Pfeil versehen
-		previous.setIcon(new ImageIcon(EditScreen.class.getResource("/de/htw/hundertwasser/res/arrow_left.png")));
-		
-		
-		
-		
-		
-		
+		previous.setIcon(new ImageIcon(EditScreen.class
+				.getResource("/de/htw/hundertwasser/res/arrow_left.png")));
 
 		// ContentPane haelt standardmaeßig ein BorderLayout
 		// Hinzufuegen der Buttons zum Content Pane des JFrames
-		centralPanel.add(previous, BorderLayout.WEST);
-		centralPanel.add(next, BorderLayout.EAST);
-		//centralPanel.add(attractivePanel, BorderLayout.CENTER);
 		controlPanel.add(infoBar, BorderLayout.NORTH);
 		controlPanel.add(toolBar, BorderLayout.CENTER);
 
