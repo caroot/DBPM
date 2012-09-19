d87a1e6a22f1ae0d27824e503377498159b12305
caroot <pib.dominic.holz@htw-saarland.de>
2012-09-19 09:53:12 +0300

    Update src/de/htw/hundertwasser/view/PhotoAlbumEditScreen.java
    added image viewer

--- src/de/htw/hundertwasser/view/PhotoAlbumEditScreen.java
+++ src/de/htw/hundertwasser/view/PhotoAlbumEditScreen.java
@@ -1,8 +1,15 @@
 package de.htw.hundertwasser.view;
 
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Dimension;
 import java.awt.event.WindowEvent;
+import java.io.IOException;
 
 import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+
+import de.htw.hundertwasser.res.RessourcenEnummeration;
 
 public class PhotoAlbumEditScreen extends EditScreen{
 	
@@ -10,9 +17,28 @@ public class PhotoAlbumEditScreen extends EditScreen{
 	private static final long serialVersionUID = 1L;
 
 
+
 	public PhotoAlbumEditScreen() {
 		
+			
 		super();
+		try {
+			imgViewer.setImage(RessourcenEnummeration.getImage(RessourcenEnummeration.ALBUM));
+		} catch (IllegalArgumentException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		JPanel mover = new JPanel();
+		mover.setPreferredSize(new Dimension(0,150));
+		mover.setBackground(Color.WHITE);
+		centralPanel.add(mover, BorderLayout.NORTH);
+
+		imgPanel.add(imgViewer,BorderLayout.CENTER);
+
 	}
 	
 	
