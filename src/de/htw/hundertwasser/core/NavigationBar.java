e4fc591b3072905af1c6c0fb54f7bba49627b8be
johannesschramm <johannesschramm@wpa-uds169-66.funklan.uni-saarland.de>
2012-09-20 12:10:54 +0200

    Navigation Bar angepasst Panel draus gemacht

--- src/de/htw/hundertwasser/core/NavigationBar.java
+++ src/de/htw/hundertwasser/core/NavigationBar.java
@@ -1,112 +1,64 @@
 package de.htw.hundertwasser.core;
 
-import java.awt.Color;
-import java.awt.Dimension;
-import java.awt.Toolkit;
-
-import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.border.MatteBorder;
+import java.awt.Color;
 import javax.swing.JButton;
 import java.awt.Font;
-import javax.swing.JScrollPane;
-import javax.swing.JLabel;
-import javax.swing.AbstractAction;
+import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
-
-import javax.swing.Action;
+import java.awt.BorderLayout;
+import javax.swing.JLabel;
 import javax.swing.JScrollBar;
+import javax.swing.SwingConstants;
+import javax.swing.JScrollPane;
 
-public class NavigationBar extends JFrame{
-	private final Action action = new SwingAction();
-	private final Action action_1 = new SwingAction_1();
-	
-	
+
+
+public class NavigationBar extends JPanel{
 	public NavigationBar() {
-		getContentPane().setFont(new Font("Calibri", Font.PLAIN, 14));
-		
-		setMinimumSize(new Dimension(Toolkit.getDefaultToolkit().getScreenSize().width,Toolkit.getDefaultToolkit().getScreenSize().height-30 ));
-		setPreferredSize(new Dimension(Toolkit.getDefaultToolkit().getScreenSize().width,Toolkit.getDefaultToolkit().getScreenSize().height-30 ));	
-		
-		setUndecorated(true);
-		setState(JFrame.MAXIMIZED_BOTH);
-		setTitle("NavigationBar");
-		getContentPane().setBackground(Color.WHITE);
-		getContentPane().setLayout(null);
+		setLayout(null);
 		
 		JPanel panel = new JPanel();
-		panel.setBorder(null);
-		panel.setBackground(Color.WHITE);
-		panel.setBounds(6, 6, 144, 408);
-		getContentPane().add(panel);
+		panel.setBorder(new MatteBorder(1, 1, 1, 1, (Color) new Color(0, 0, 0)));
+		panel.setBounds(6, 6, 141, 288);
+		add(panel);
 		panel.setLayout(null);
 		
-		/**
-		 *  Photobox panel ----------------------------------------------------------------------------------------------
-		 */
+		JPanel panel_1 = new JPanel();
+		panel_1.setBorder(new MatteBorder(1, 1, 1, 1, (Color) new Color(0, 0, 0)));
+		panel_1.setBounds(6, 6, 129, 115);
+		panel.add(panel_1);
+		panel_1.setLayout(new BorderLayout(0, 0));
 		
-		JScrollPane scrollPane = new JScrollPane();
-		scrollPane.setBounds(6, 6, 130, 162);
-		panel.add(scrollPane);
+		JLabel lblYourPhotoAlbums = new JLabel("      Your photo boxes");
+		lblYourPhotoAlbums.setVerticalAlignment(SwingConstants.TOP);
+		lblYourPhotoAlbums.setFont(new Font("Calibri", Font.PLAIN, 11));
+		panel_1.add(lblYourPhotoAlbums);
 		
-		JLabel lblYourPhotoBoxes = new JLabel("      Your Photo Boxes");
-		lblYourPhotoBoxes.setFont(new Font("Calibri", Font.PLAIN, 12));
-		lblYourPhotoBoxes.setBackground(Color.WHITE);
-		scrollPane.setColumnHeaderView(lblYourPhotoBoxes);
+		JPanel panel_2 = new JPanel();
+		panel_2.setBorder(new MatteBorder(1, 1, 1, 1, (Color) new Color(0, 0, 0)));
+		panel_2.setBounds(6, 149, 129, 115);
+		panel.add(panel_2);
+		panel_2.setLayout(new BorderLayout(0, 0));
 		
-		JScrollPane scrollPane_2 = new JScrollPane();
-		scrollPane.setViewportView(scrollPane_2);
-
-		JButton btnNewButton = new JButton("+New PhotoBox");
-		btnNewButton.setAction(action);
+		JLabel lblYourPhotoAlbums_1 = new JLabel("     Your photo albums");
+		lblYourPhotoAlbums_1.setVerticalAlignment(SwingConstants.TOP);
+		lblYourPhotoAlbums_1.setFont(new Font("Calibri", Font.PLAIN, 11));
+		panel_2.add(lblYourPhotoAlbums_1);
+		
+		JButton btnNewButton = new JButton("+new photoalbum");
 		btnNewButton.setFont(new Font("Calibri", Font.PLAIN, 10));
-		btnNewButton.setBounds(16, 173, 110, 27);
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+			}
+		});
+		btnNewButton.setBounds(16, 122, 100, 24);
 		panel.add(btnNewButton);
 		
-		/**
-		 * 	Photo album panel ----------------------------------------------------------------------------------------------
-		 */
-		
-		JScrollPane scrollPane_1 = new JScrollPane();
-		scrollPane_1.setBounds(6, 204, 130, 162);
-		panel.add(scrollPane_1);
-		
-		JLabel lblNewLabel = new JLabel("      Your Photo Albums");
-		lblNewLabel.setFont(new Font("Calibri", Font.PLAIN, 12));
-		lblNewLabel.setBackground(Color.WHITE);
-		scrollPane_1.setColumnHeaderView(lblNewLabel);
-		
-		JScrollPane scrollPane_3 = new JScrollPane();
-		scrollPane_1.setViewportView(scrollPane_3);
-		
-	    JButton btnPhotobox = new JButton("+ New PhotoAlbum");
-	    btnPhotobox.setAction(action_1);
-	    btnPhotobox.setFont(new Font("Calibri", Font.PLAIN, 10));
-	    btnPhotobox.setBounds(16, 375, 110, 27);
-	    panel.add(btnPhotobox);
-	}
-		
-	/**
-	 * action of the two buttons used in the bar
-	 * 
-	 * @author johannesschramm
-	 *
-	 */
-	private class SwingAction extends AbstractAction {
-		public SwingAction() {
-			putValue(NAME, "+ new Photobox");
-			putValue(SHORT_DESCRIPTION, "add PhotoBox");
-		}
-		public void actionPerformed(ActionEvent e) {
-			// Aufruf des Photobox Konstruktors
-		}
-	}
-	private class SwingAction_1 extends AbstractAction {
-		public SwingAction_1() {
-			putValue(NAME, "+ new Photoalbum");
-			putValue(SHORT_DESCRIPTION, "add Photoalbum");
-		}
-		public void actionPerformed(ActionEvent e) {
-			// Aufruf des Photoalbum Konstruktors
-		}
+		JButton btnNewButton_1 = new JButton("+new photoalbum");
+		btnNewButton_1.setBounds(16, 264, 100, 24);
+		panel.add(btnNewButton_1);
+		btnNewButton_1.setFont(new Font("Calibri", Font.PLAIN, 10));
 	}
 }
