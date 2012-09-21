f8a30235e41587c24b659d8bf655a9937dc05196
Ramis <Ramis@134.96.168.160>
2012-09-21 14:48:38 +0200

    Kommentar added

--- src/de/htw/hundertwasser/view/PhotoBoxFullScreen.java
+++ src/de/htw/hundertwasser/view/PhotoBoxFullScreen.java
@@ -1,62 +1,49 @@
-
-
 package de.htw.hundertwasser.view;
-
-
-
-/**
- * Klasse die zum anzeigen des gewünschten Fotoalbums im Fullscreen Modus genutzt wird
- * @author johannes
- *
- */
-
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import de.htw.hundertwasser.core.ImageViewer;
 
-
-
 /**
- * Klasse die zum anzeigen des gewünschten Fotoboxenim Fullscreen Modus genutzt wird
- * @author johannes
- *
+ * 
+ * Class that creates the PhotoBoxFullScreen
+ * 
+ * @author johannes schramm
+ * 
  */
 
 public class PhotoBoxFullScreen extends FullScreen {
-	
 
 	private static final long serialVersionUID = 1L;
 
-	public PhotoBoxFullScreen (ImageViewer iv){
+	public PhotoBoxFullScreen(ImageViewer iv) {
 		super(iv);
 		exitButton.addActionListener(getExitButtonListener());
 		leftArrow.addActionListener(getLeftArrowListener());
 		rightArrow.addActionListener(getRightArrowListener());
 	}
-	
+
 	// exitButton
 	public ActionListener getExitButtonListener() {
-		return new ActionListener(){
+		return new ActionListener() {
 
 			public void actionPerformed(ActionEvent arg0) {
-				dispose();}
+				dispose();
+			}
 		};
 	}
 
-
 	// right arrow
-	
+
 	public ActionListener getRightArrowListener() {
 		return null;
-   }
+	}
 
 	// left arrow
-	
+
 	public ActionListener getLeftArrowListener() {
-		
+
 		return null;
 	}
 }
-
