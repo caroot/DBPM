5283f4f3785b4cfad7e56d541dbe29364df1941e
Ramis <Ramis@134.96.168.49>
2012-09-21 09:18:23 +0200

    Kommentare angepasst

--- src/de/htw/hundertwasser/backend/ImageManager.java
+++ src/de/htw/hundertwasser/backend/ImageManager.java
@@ -12,8 +12,9 @@ import de.htw.hundertwasser.custom.error.InsufficientPrivilegesException;
 
 /**
  * This class will manage the Images of the current Project
- * @author daniel
- *
+ * 
+ * @author daniel rhein
+ * 
  */
 public class ImageManager {
 
@@ -21,59 +22,65 @@ public class ImageManager {
 
 	private static final String ERROR_EMPTY_PATH = "The path can't be empty.";
 	private static final String ERROR_NULL_PATH = "The path can't be null.";
-	
-	
-	public ImageManager()
-	{
-		
+
+	public ImageManager() {
+
 	}
-	
+
 	/**
-	 * Lese das Bild aus einer Datei
-	 * @param absouluteFile Der absoulute Pfad zur Datei.
+	 * Read the Picture from source
+	 * 
+	 * @param absouluteFile absolute path to the file
 	 * @return picture of the image.
-	 * @throws IOException Wird geworfen wenn das Bild nicht gefunden werden kann.
-	 * @throws FileNotFoundException Wird geworfen wenn das Bild nicht gefunden werden kann.
-	 * @throws IllegalArgumentException Wenn die Datei leer oder null ist.
+	 * @throws IOException thrown if the Picture was not found
+	 * @throws FileNotFoundException thrown if the Picture was not found
+	 * @throws IllegalArgumentException thrown if the File is null
 	 */
-	public BufferedImage getImage(String absoluteFile) throws IOException,FileNotFoundException,InsufficientPrivilegesException
-	{
-		if (absoluteFile== null) throw new IllegalArgumentException(ERROR_NO_FILE_PATH);
-		if (absoluteFile.trim().isEmpty()) throw new IllegalArgumentException("The path of the file can't be emtpy.");
+	public BufferedImage getImage(String absoluteFile) throws IOException,
+			FileNotFoundException, InsufficientPrivilegesException {
+		if (absoluteFile == null)
+			throw new IllegalArgumentException(ERROR_NO_FILE_PATH);
+		if (absoluteFile.trim().isEmpty())
+			throw new IllegalArgumentException(
+					"The path of the file can't be emtpy.");
 		File file = new File(absoluteFile);
-		if (!file.exists()) throw new FileNotFoundException("File" + absoluteFile + " can't be found on your System.");
-		if (!file.canRead()) throw new InsufficientPrivilegesException("I'm not allowed to open the file" + absoluteFile);
+		if (!file.exists())
+			throw new FileNotFoundException("File" + absoluteFile
+					+ " can't be found on your System.");
+		if (!file.canRead())
+			throw new InsufficientPrivilegesException(
+					"I'm not allowed to open the file" + absoluteFile);
 		return ImageIO.read(file);
 	}
-	
+
 	/**
 	 * Returns an ArrayList of ImageFiles within the current directory
+	 * 
 	 * @param Path
 	 * @return
 	 */
-	public File[] getImagesListInFolder(String path) throws IllegalArgumentException,ChoosenFileNotAFolderException,FileNotFoundException
-	{
-		//TODO:Evtl. Rekursives auslesen notwendig.
-		File imageList =null;
+	public File[] getImagesListInFolder(String path)
+			throws IllegalArgumentException, ChoosenFileNotAFolderException,
+			FileNotFoundException {
+		// TODO:Evtl. Rekursives auslesen notwendig.
+		File imageList = null;
 		FileAcceptor fileAcceptor = new FileAcceptor();
-		if (path==null) throw new IllegalArgumentException(ERROR_NULL_PATH);
-		if (path.trim().isEmpty()) throw new IllegalArgumentException(ERROR_EMPTY_PATH);
-		
-			imageList = new File(path);
-			if (imageList.exists())
-			{
-				if (imageList.isDirectory())
-				{
+		if (path == null)
+			throw new IllegalArgumentException(ERROR_NULL_PATH);
+		if (path.trim().isEmpty())
+			throw new IllegalArgumentException(ERROR_EMPTY_PATH);
+
+		imageList = new File(path);
+		if (imageList.exists()) {
+			if (imageList.isDirectory()) {
 				return imageList.listFiles(fileAcceptor);
-				}
-				else
-				{
-					throw new ChoosenFileNotAFolderException("The given path " + path + " is not a Folder.");
-				}
+			} else {
+				throw new ChoosenFileNotAFolderException("The given path "
+						+ path + " is not a Folder.");
 			}
-			else
-			{
-				throw new FileNotFoundException("The file" + path + " doesn't exists.");
-			}
+		} else {
+			throw new FileNotFoundException("The file" + path
+					+ " doesn't exists.");
+		}
 	}
 }
