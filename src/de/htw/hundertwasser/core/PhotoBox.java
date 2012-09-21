105e58eceea726109bb39cb9b5e1b6a2da44ce61
Ramis <Ramis@134.96.168.160>
2012-09-21 12:57:34 +0200

    Kommentare hinzugefügt

--- src/de/htw/hundertwasser/core/PhotoBox.java
+++ src/de/htw/hundertwasser/core/PhotoBox.java
@@ -7,10 +7,11 @@ import java.util.ArrayList;
  * 
  * 
  * @author daniel
- *
+ * 
  */
-//TODO: PhotoBox testen; Name der PhotoBox festlegen;
-//TODO:Kommentare Einfügen; ExceptionHandling + (Implementierung des Fehlerwurfs)
+// TODO: PhotoBox testen; Name der PhotoBox festlegen;
+// TODO:Kommentare Einfügen; ExceptionHandling + (Implementierung des
+// Fehlerwurfs)
 
 public class PhotoBox {
 
@@ -20,94 +21,110 @@ public class PhotoBox {
 	private static final String ERROR_PHOTO_NULL = "photo can't be null";
 	private static final String ERROR_EMPTY_NAME = "name can't be empty.";
 	private static final String ERROR_NAME_NULL = "name can't be null";
-	private String name="";
+	private String name = "";
 	private ArrayList<Photo> arPhotos;
 	private String absolutePathToFiles;
+
 	/**
-	 * Konstruktor
+	 * Constructor
+	 * 
 	 * @throws IllegalArgumentExceptoin
 	 */
-	public PhotoBox(String absoultePathToFiles) throws IllegalArgumentException
-	{
-		arPhotos= new ArrayList<Photo>();
+	public PhotoBox(String absoultePathToFiles) throws IllegalArgumentException {
+		arPhotos = new ArrayList<Photo>();
 	}
-	
+
 	/**
 	 * Get the Absolute Path to File
+	 * 
 	 * @return
 	 */
 	public String getAbsolutePathToFiles() {
 		return absolutePathToFiles;
 	}
+
 	/**
 	 * Set AbsolutePath to file
+	 * 
 	 * @param absolutePathToFiles
 	 */
 	public void setAbsolutePathToFiles(String absolutePathToFiles) {
 		this.absolutePathToFiles = absolutePathToFiles;
 	}
+
 	/**
-	 * Name of the  PhotoBox
+	 * Name of the PhotoBox
+	 * 
 	 * @return
 	 */
 	public String getName() {
 		return name;
 	}
-	
+
 	/**
 	 * Set the Name of the Photobox
+	 * 
 	 * @param name
 	 * @throws IllegalArgumentException
 	 */
 	public void setName(String name) throws IllegalArgumentException {
-		if (name== null) throw new IllegalArgumentException(ERROR_NAME_NULL);
-		if (name.trim().isEmpty())  throw new IllegalArgumentException(ERROR_EMPTY_NAME);
+		if (name == null)
+			throw new IllegalArgumentException(ERROR_NAME_NULL);
+		if (name.trim().isEmpty())
+			throw new IllegalArgumentException(ERROR_EMPTY_NAME);
 		this.name = name;
 	}
-	
+
 	/**
 	 * Add a Photo to the current Photobox
+	 * 
 	 * @param photo
 	 * @throws IllegalArgumentException
 	 */
-	public void addPhoto(Photo photo) throws IllegalArgumentException
-	{
-		if (photo==null) throw new IllegalArgumentException(ERROR_PHOTO_NULL);
+	public void addPhoto(Photo photo) throws IllegalArgumentException {
+		if (photo == null)
+			throw new IllegalArgumentException(ERROR_PHOTO_NULL);
 		arPhotos.add(photo);
 	}
+
 	/**
 	 * Returns the amount of Photos within the box.
+	 * 
 	 * @return
 	 */
-	public int getCount()
-	{
+	public int getCount() {
 		return arPhotos.size();
 	}
-	
+
 	/**
 	 * Returns a Photo of the Photobox
+	 * 
 	 * @param id
 	 * @return
 	 * @throws IllegalArgumentException
 	 * @throws IndexOutOfBoundsException
 	 */
-	public Photo getPhoto(int id) throws IllegalArgumentException,IndexOutOfBoundsException
-	{
-		if (id<MIN_ID) throw new IllegalArgumentException(ERROR_ID_NEGATIVE);
-		if (id>=arPhotos.size()) throw new IndexOutOfBoundsException(ERROR_INDEX_OUT_OF_BOUNDS);
+	public Photo getPhoto(int id) throws IllegalArgumentException,
+			IndexOutOfBoundsException {
+		if (id < MIN_ID)
+			throw new IllegalArgumentException(ERROR_ID_NEGATIVE);
+		if (id >= arPhotos.size())
+			throw new IndexOutOfBoundsException(ERROR_INDEX_OUT_OF_BOUNDS);
 		return arPhotos.get(id);
 	}
-	
+
 	/**
 	 * Remove the photo form a Photobox
+	 * 
 	 * @param id
 	 * @return
 	 * @throws IllegalArgumentException
 	 */
-	public boolean removePhoto(int id) throws IllegalArgumentException
-	{
-		if (id<MIN_ID) throw new IllegalArgumentException(ERROR_ID_NEGATIVE);
-		if (id>=arPhotos.size()) throw new IndexOutOfBoundsException(ERROR_INDEX_OUT_OF_BOUNDS);
+	public boolean removePhoto(int id) throws IllegalArgumentException {
+		if (id < MIN_ID)
+			throw new IllegalArgumentException(ERROR_ID_NEGATIVE);
+		if (id >= arPhotos.size())
+			throw new IndexOutOfBoundsException(ERROR_INDEX_OUT_OF_BOUNDS);
 		arPhotos.remove(id);
 		return true;
 	}
