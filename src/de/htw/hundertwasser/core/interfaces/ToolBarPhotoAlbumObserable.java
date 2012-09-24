35fb3d1d040a009f15b1815665435c5bead63bae
Steffen <Steffen@Steffens-Laptop>
2012-09-24 15:45:20 +0200

    Observer und Observerable für die Toolbar

--- src/de/htw/hundertwasser/core/interfaces/ToolBarPhotoAlbumObserable.java
+++ src/de/htw/hundertwasser/core/interfaces/ToolBarPhotoAlbumObserable.java
@@ -1,6 +1,5 @@
 package de.htw.hundertwasser.core.interfaces;
 
-import de.htw.hundertwasser.core.Photo;
 import de.htw.hundertwasser.core.PhotoAlbum;
 
 /**
@@ -21,45 +20,9 @@ public interface ToolBarPhotoAlbumObserable {
 	void removeToolBarPhotoAlbumObserver(ToolBarPhotoAlbumObserver observer);
 	
 	/**
-	 * FullScreenMessage 
+	 * send message to all observers
 	 * @param photoAlbum
-	 */
-	void fullScreenMessage(PhotoAlbum photoAlbum);
-	/**
-	 * Changes the Color of the PhotoAlbum to Black and White
-	 * @param photoAlbum
-	 */
-	void blackAndWhiteMessage(PhotoAlbum photoAlbum);
-	/**
-	 * Rename the PhotoAlbum to a certain name 
-	 * @param photoAlbum
-	 */
-	void renameMessage(PhotoAlbum photoAlbum);
-	/**
-	 * ZoomMessage creates a Message to zoom into the PhotoAlbum
-	 * @param photoAlbum
-	 */
-	void zoomMessage(PhotoAlbum photoAlbum);
-	/**
-	 * CutMessage creates a Message to Cut the PhotoAlbum
-	 * @param photoAlbum
-	 */
-	void cutMessage(PhotoAlbum photoAlbum);
-	/**
-	 * Creates a Message to Print the eternal PhotoAlbum
-	 * @param photoAlbum
-	 */
-	void printMessage(PhotoAlbum photoAlbum);
-	/**
-	 * Creates a Message  to Delete the PhotoAlbum
-	 * @param photoAlbum
-	 */
-	void deleteMessage(PhotoAlbum photoAlbum);
-	/**
-	 * Send a Certain Message to all collaborators
 	 * @param message
-	 * @param photoAlbum
 	 */
-	void sendMessage(ToolBarObserversMessage message,PhotoAlbum photoAlbum);
-	
-}
+	void sendMessage(PhotoAlbum photoAlbum, ToolBarObserversMessage message);
+	}
--- src/de/htw/hundertwasser/core/interfaces/ToolBarPhotoAlbumObserver.java
+++ src/de/htw/hundertwasser/core/interfaces/ToolBarPhotoAlbumObserver.java
@@ -1,5 +1,14 @@
 package de.htw.hundertwasser.core.interfaces;
 
+import de.htw.hundertwasser.core.PhotoAlbum;
+
 public interface ToolBarPhotoAlbumObserver {
 
+	/**
+	 * This methode transmitts the photoalbum and the message to the observers
+	 * 
+	 * @param photoalbum
+	 * @param message
+	 */
+	public void receiveMessage(PhotoAlbum photoalbum, ToolBarObserversMessage message);
 }
