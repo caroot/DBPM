b2f316c26ff17fe8d861fcd64ad647b93225962d
Ramis <Ramis@134.96.169.38>
2012-09-24 09:37:48 +0200

    kommentar geändert

--- src/de/htw/hundertwasser/core/NavBarPhotoAlbumRenderer.java
+++ src/de/htw/hundertwasser/core/NavBarPhotoAlbumRenderer.java
@@ -9,35 +9,39 @@ import javax.swing.JPanel;
 import javax.swing.JTree;
 import javax.swing.tree.TreeCellRenderer;
 
-public class NavBarPhotoAlbumRenderer extends JPanel implements TreeCellRenderer{
+/**
+ * Class that describes the Renderer
+ * 
+ * @author Daniel Rhein
+ * 
+ */
+public class NavBarPhotoAlbumRenderer extends JPanel implements
+		TreeCellRenderer {
 
-	/**
-	 * 
-	 */
 	private static final long serialVersionUID = 1L;
 	private JLabel lblText;
-//	private ImageViewer iv;
-	
-	
-	public NavBarPhotoAlbumRenderer() throws IOException
-	{
-//		lblText=new JLabel("",RessourcenEnummeration.PHOTOALBUM.getIcon(),JLabel.LEADING);
-		lblText=new JLabel("");
-//		iv = new ImageViewer();
-		setLayout(new BorderLayout(10,10));
-		add(lblText,BorderLayout.CENTER);
-//		add(iv,BorderLayout.EAST);
+
+	// private ImageViewer iv;
+
+	public NavBarPhotoAlbumRenderer() throws IOException {
+		// lblText=new
+		// JLabel("",RessourcenEnummeration.PHOTOALBUM.getIcon(),JLabel.LEADING);
+		lblText = new JLabel("");
+		// iv = new ImageViewer();
+		setLayout(new BorderLayout(10, 10));
+		add(lblText, BorderLayout.CENTER);
+		// add(iv,BorderLayout.EAST);
 	}
+
 	@Override
 	public Component getTreeCellRendererComponent(JTree tree, Object value,
-			boolean selected, boolean expanded, boolean leaf, int row, boolean hasFocus) {
-		if (value instanceof PhotoAlbum)
-		{
-			PhotoAlbum photoalbum = (PhotoAlbum)value;
+			boolean selected, boolean expanded, boolean leaf, int row,
+			boolean hasFocus) {
+		if (value instanceof PhotoAlbum) {
+			PhotoAlbum photoalbum = (PhotoAlbum) value;
 			lblText.setText(photoalbum.getName());
 		}
-		if (value instanceof String)
-		{
+		if (value instanceof String) {
 			lblText.setText((String) value);
 		}
 		return this;
