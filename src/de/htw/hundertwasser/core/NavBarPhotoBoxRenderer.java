2f3822fb0f9ce2eb36ab2409bbeda5a49a0a91d1
Ramis <Ramis@134.96.169.38>
2012-09-24 09:38:51 +0200

    Kommentare geändert

--- src/de/htw/hundertwasser/core/NavBarPhotoBoxRenderer.java
+++ src/de/htw/hundertwasser/core/NavBarPhotoBoxRenderer.java
@@ -1,9 +1,3 @@
-/**
- * @author Dominic Holz (template by Daniel Rhein)
- *
- */
-
-
 package de.htw.hundertwasser.core;
 
 import java.awt.BorderLayout;
@@ -15,35 +9,36 @@ import javax.swing.JPanel;
 import javax.swing.JTree;
 import javax.swing.tree.TreeCellRenderer;
 
-public class NavBarPhotoBoxRenderer extends JPanel implements TreeCellRenderer{
+/**
+ * @author Dominic Holz (template by Daniel Rhein)
+ * 
+ */
+public class NavBarPhotoBoxRenderer extends JPanel implements TreeCellRenderer {
 
-	/**
-	 * 
-	 */
 	private static final long serialVersionUID = 1L;
 	private JLabel lblText;
-//	private ImageViewer iv;
-	
-	
-	public NavBarPhotoBoxRenderer() throws IOException
-	{
-//		lblText=new JLabel("",RessourcenEnummeration.PHOTOBOX.getIcon(),JLabel.LEADING);
-		lblText=new JLabel("");
-//		iv = new ImageViewer();
-		setLayout(new BorderLayout(10,10));
-		add(lblText,BorderLayout.CENTER);
-//		add(iv,BorderLayout.EAST);
+
+	// private ImageViewer iv;
+
+	public NavBarPhotoBoxRenderer() throws IOException {
+		// lblText=new
+		// JLabel("",RessourcenEnummeration.PHOTOBOX.getIcon(),JLabel.LEADING);
+		lblText = new JLabel("");
+		// iv = new ImageViewer();
+		setLayout(new BorderLayout(10, 10));
+		add(lblText, BorderLayout.CENTER);
+		// add(iv,BorderLayout.EAST);
 	}
+
 	@Override
 	public Component getTreeCellRendererComponent(JTree tree, Object value,
-			boolean selected, boolean expanded, boolean leaf, int row, boolean hasFocus) {
-		if (value instanceof PhotoAlbum)
-		{
-			PhotoBox photobox = (PhotoBox)value;
+			boolean selected, boolean expanded, boolean leaf, int row,
+			boolean hasFocus) {
+		if (value instanceof PhotoAlbum) {
+			PhotoBox photobox = (PhotoBox) value;
 			lblText.setText(photobox.getName());
 		}
-		if (value instanceof String)
-		{
+		if (value instanceof String) {
 			lblText.setText((String) value);
 		}
 		return this;
