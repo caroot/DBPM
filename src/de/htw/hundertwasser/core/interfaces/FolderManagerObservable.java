f3175ca08c56feb00bf8a00c5b5ef32603d65bb1
pril <daniel.rhein84@googlemail.com>
2012-09-25 10:18:37 +0200

    Observer implementiert.

---
+++ src/de/htw/hundertwasser/core/interfaces/FolderManagerObservable.java
@@ -0,0 +1,22 @@
+package de.htw.hundertwasser.core.interfaces;
+
+import de.htw.hundertwasser.core.PhotoBox;
+
+public interface FolderManagerObservable {
+
+	/**
+	 * Add FolderManagerObserver
+	 * @param observer
+	 */
+	void addFolderManagerObserver(FolderManagerObserver observer);
+	/**
+	 * Removes the FolderManagerObserver
+	 * @param observer
+	 */
+	void removeFolderManagerObserver(FolderManagerObserver observer);
+	/**
+	 * Send Message to All Observers
+	 * @param photoBox
+	 */
+	void sendFolderManagerMessage(PhotoBox photoBox);
+}
---
+++ src/de/htw/hundertwasser/core/interfaces/FolderManagerObserver.java
@@ -0,0 +1,10 @@
+package de.htw.hundertwasser.core.interfaces;
+
+import de.htw.hundertwasser.core.PhotoBox;
+
+public interface FolderManagerObserver {
+
+	
+	void importNewPhotoBox(PhotoBox photoBox);
+	
+}
